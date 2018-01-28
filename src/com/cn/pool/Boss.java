ccac379223090b0e900e32f7b6f467f7ef72a6f3
guhongboe <2978032936@qq.com>
2018-01-28 22:57:14 +0800

    first commit

---
+++ src/com/cn/pool/Boss.java
@@ -0,0 +1,12 @@
+package com.cn.pool;
+
+import java.nio.channels.ServerSocketChannel;
+
+public interface Boss {
+	
+	/**
+	 * 加入一个新的ServerSocket
+	 * @param serverChannel
+	 */
+	public void registerAcceptChannelTask(ServerSocketChannel serverChannel);
+}
---
+++ src/com/cn/pool/Worker.java
@@ -0,0 +1,13 @@
+package com.cn.pool;
+
+import java.nio.channels.SocketChannel;
+
+public interface Worker {
+	
+	/**
+	 * 加入一个新的客户端会话
+	 * @param channel
+	 */
+	public void registerNewChannelTask(SocketChannel channel);
+
+}
